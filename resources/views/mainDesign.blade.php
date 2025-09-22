d15dd37dea32f932bb94bb11a004a650fa8b07b7
hamzaayachi <hamza.ayachi.pro@gmail.com>
2025-09-22 17:23:24 +0100

    create authentification with laravel breeze and add in tamplate (login and lgout)

--- resources/views/mainDesign.blade.php
+++ resources/views/mainDesign.blade.php
@@ -82,9 +82,27 @@
             <li class="nav-item">
               <a class="nav-link" href="contact.html">Contact</a>
             </li>
-            <li class="nav-item">
-              <a class="btn btn-primary ml-lg-3" href="#">Login / Register</a>
+
+            @if(!Auth::check())
+                
+             <li class="nav-item">
+              <a href="{{ route('login') }}">Login</a>
             </li>
+
+            @else
+            <li class="nav-item">
+              
+               <form method="POST" action="{{ route('logout') }}">
+                            @csrf
+
+                            <x-dropdown-link :href="route('logout')"
+                                    onclick="event.preventDefault();
+                                                this.closest('form').submit();">
+                                {{ __('Log Out') }}
+                            </x-dropdown-link>
+                        </form>
+            </li>
+            @endif
           </ul>
         </div> <!-- .navbar-collapse -->
       </div> <!-- .container -->
